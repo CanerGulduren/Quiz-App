d69c89d778c57b70ef9dedb02c7aef679061646a
Caner Güldüren <canerguldurenn@gmail.com>
2024-06-30 08:54:28 +0300

    Add funtion to change selected category
    - Added changeCategory function to update category ID and navigate to difficulty page.

--- app/components/category/CategoryCard.jsx
+++ app/components/category/CategoryCard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client"
+import React, {useContext} from 'react'
+import Image from 'next/image'
+import { QuizContext } from '@/app/helpers/context/QuizProvider';
+import { useRouter } from 'next/navigation';
 
-function CategoryCard() {
+function CategoryCard({href, title, id}) {
+  const {setCategoryID} = useContext(QuizContext);
+  const router = useRouter();
+
+  const changeCategory = () => {
+    const categoryID = id.toString();
+    setCategoryID(categoryID);
+    router.push("/pages/difficulty")
+  }
+
   return (
-    <div>
-        
+    <div onClick={changeCategory}>
+        <Image 
+        src={href} 
+        alt={`${title} icon`}
+        width={100} 
+        height={100} 
+        />
+        <h5>{title}</h5>
     </div>
   )
 }
