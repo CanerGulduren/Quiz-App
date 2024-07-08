a73a35fefb7b08e45e3e0eeba87a1a980994fba2
Caner Güldüren <canerguldurenn@gmail.com>
2024-07-08 13:08:04 +0300

    Add index file in helpers directory for easy access
    - Created an index file in the helpers directory to facilitate access to utility functions.
    - Exported the imported functions to provide a centralized access point for other components.

---
+++ app/helpers/index.js
@@ -0,0 +1,5 @@
+import { fetchRequest } from "./api/fetch";
+import { newTokenRequest } from "./api/token";
+import { decodeQuizData } from "./api/decode";
+
+export {fetchRequest, newTokenRequest, decodeQuizData}
